79fcf771a3853635e7959f1acac582c3e206e780
Christof Schulze <christof.schulze@gmx.net>
2015-12-23 01:29:46 +0100

    imagecreate lädt sich nun das raspbian-image bei bedarf herunter

--- imagebuilder/imagecreate.sh
+++ imagebuilder/imagecreate.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-baseimage=/var/lib/jenkins/2015-11-21-raspbian-jessie-lite.img.org
-image=2015-11-21-raspbian-jessie-lite_rebuilt-$(date -I).img
-cp $baseimage $image
+
+dlfile=raspbian_lite_latest
+[[ -f $dlfile ]] || wget -O $dlfile https://downloads.raspberrypi.org/raspbian_lite_latest
+
+#find baseimage in this ugly zip output
+while read
+do
+	case $REPLY in
+	*inflating*.img*) baseimage=${REPLY##*: };;
+	*);;
+	esac
+done < <(unzip -o $dlfile)
+
+image=${baseimage}_rebuilt-$(date -I).img
+mv $baseimage $image
 
 startsect=$(/sbin/fdisk  -lu $image|grep img2|awk '{print $2}')
 lodev=$(sudo /sbin/losetup --show -f -o $((startsect*512)) $image)
@@ -10,12 +22,11 @@ lodev=$(sudo /sbin/losetup --show -f -o $((startsect*512)) $image)
 dd if=/dev/zero of=$image bs=2M oflag=append conv=notrunc count=1024
 lastsect=$(echo $(/sbin/fdisk  -lu $image|grep  $image|grep sectors|rev|cut -d" " -f2|rev) -1|bc -l)
 
-
 # Partitionstabelle bearbeiten: 2. Partition (rootfs) löschen und an der selben Position neu anlegen. 
 # Endsektor ist der letzte Sektor des Images
 # http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
+# danach fs vergrößern und Dateibaum vom nfsroot reinsynchronisieren
 echo -e "\nd\n2\nn\np\n2\n${startsect}\n${lastsect}\np\nw\nq" | /sbin/fdisk $image
-
 lodev=$(sudo /sbin/losetup --show -f -o $((startsect*512)) $image)
 sudo /sbin/e2fsck -fy $lodev
 sudo /sbin/resize2fs $lodev
@@ -30,4 +41,5 @@ sudo rsync -aAxl --delete /tmp/sp/ /tmp/ip/|| echo some files could not be trans
 sudo /bin/umount /tmp/sp && rm -rf /tmp/sp
 sudo /bin/umount /tmp/ip && rm -rf /tmp/ip
 sudo /sbin/losetup -d $lodev
-cat $image |lzma -6 >$image.lzma
+cat $image |lzma -7 >$image.lzma
+
