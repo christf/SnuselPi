010e767d086af0d111a7f25ae239cbeee1e71622
Christof Schulze <christof.schulze@gmx.net>
2015-12-23 10:30:31 +0100

    Fehlerbehandlung

--- imagebuilder/imagecreate.sh
+++ imagebuilder/imagecreate.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
+nfslocation=freenas:/mnt/data/rootnfs/snuselpi 
+nfspath=/tmp/sp
+imagepath=/tmp/ip
+dlurl=https://downloads.raspberrypi.org/raspbian_lite_latest
+
+
+trapfunc()
+{
+	cleanup
+	exit 1
+}
+
+cleanup()
+{
+	sudo /bin/umount $nfspath && rm -rf $nfspath
+	sudo /bin/umount $imagepath && rm -rf $imagepath 
+	sudo /sbin/losetup -d $lodev
+}
+
+trap 'trapfunc' 1 2 3 6 9 13 14 15
 
 dlfile=raspbian_lite_latest
-[[ -f $dlfile ]] || wget -O $dlfile https://downloads.raspberrypi.org/raspbian_lite_latest
-
+[[ -f $dlfile ]] || wget -O $dlfile $dlurl
 #find baseimage in this ugly zip output
 while read
 do
 	case $REPLY in
-	*inflating*.img*) baseimage=${REPLY##*: };;
+	*inflating*) baseimage=$(echo ${REPLY##*: } |tr -dc [1-9,a-z,A-Z,_,-]);;
 	*);;
-	esac
+esac
 done < <(unzip -o $dlfile)
-
-image=${baseimage}_rebuilt-$(date -I).img
+set -ix
+image="${baseimage}_rebuilt-$(date -I).img"
 mv $baseimage $image
 
 startsect=$(/sbin/fdisk  -lu $image|grep img2|awk '{print $2}')
@@ -26,20 +45,19 @@ lastsect=$(echo $(/sbin/fdisk  -lu $image|grep  $image|grep sectors|rev|cut -d" 
 # Endsektor ist der letzte Sektor des Images
 # http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
 # danach fs vergrößern und Dateibaum vom nfsroot reinsynchronisieren
-echo -e "\nd\n2\nn\np\n2\n${startsect}\n${lastsect}\np\nw\nq" | /sbin/fdisk $image
+echo -e "\np\nd\n2\nn\np\n2\n${startsect}\n${lastsect}\np\nw\nq" | /sbin/fdisk $image
 lodev=$(sudo /sbin/losetup --show -f -o $((startsect*512)) $image)
 sudo /sbin/e2fsck -fy $lodev
 sudo /sbin/resize2fs $lodev
 
-mkdir -p /tmp/ip
-mkdir -p /tmp/sp
-sudo /bin/mount freenas:/mnt/data/rootnfs/snuselpi /tmp/sp
-sudo /bin/mount $lodev /tmp/ip
+mkdir -p $imagepath
+mkdir -p $nfspath
+sudo /bin/mount $nfslocation $nfspath || trapfunc
+sudo /bin/mount $lodev $imagepath || trapfunc
 
-sudo rsync -aAxl --delete /tmp/sp/ /tmp/ip/|| echo some files could not be transferred.
+sudo rsync -aAxl --delete $nfspath $imagepath || echo some files could not be transferred.
 
-sudo /bin/umount /tmp/sp && rm -rf /tmp/sp
-sudo /bin/umount /tmp/ip && rm -rf /tmp/ip
-sudo /sbin/losetup -d $lodev
-cat $image |lzma -7 >$image.lzma
+cleanup
+lzma -7 $image
 
+trap - 1 2 3 6 9 13 14 15
