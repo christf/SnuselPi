8dae1dc8bcb71da8be779730fcb0eefc4926f4eb
Christof Schulze <christof.schulze@gmx.net>
2015-12-28 00:14:36 +0100

    allow files and directories as input

--- weckzeitfinder/src/main/java/com/penguineering/snuselpi/Weckzeitfinder.java
+++ weckzeitfinder/src/main/java/com/penguineering/snuselpi/Weckzeitfinder.java
@@ -3,9 +3,6 @@ package com.penguineering.snuselpi;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Iterator;
 
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemManager;
@@ -16,54 +13,74 @@ import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.Period;
+import net.fortuna.ical4j.model.PeriodList;
 import net.fortuna.ical4j.model.Property;
 
 public class Weckzeitfinder {
 
-	private static boolean isinbetween(Component component, Date lowerdate, Date upperdate) {
+	private static boolean isinbetween(Component component, DateTime start, DateTime end) {
+
+		// DateTime start = new DateTime(event.getStartDate().getDate());
+		// DateTime end = new DateTime(calEnd.getTime());
+		Period period = new Period(start, end);
+		PeriodList r = component.calculateRecurrenceSet(period);
+		System.out.println(r);
 		return false;
 
 	}
 
 	public static void main(String[] args)
 			throws InvalidRecurrenceRuleException, IOException, FileNotFoundException, ParserException {
-
-		int limit = 10;
 		// IcsProcessor ip = new IcsProcessor();
-		DateFormat lowerdatetime, upperdatetime;
-lowerdatetime = new DateFormat()
-		System.setProperty("ical4j.unfolding.relaxed", "true");
+		DateTime lowerdatetime, upperdatetime;
 
-		String filename = new String(
-				"/home/christof/.calendars/christof/bd2bba8e-ba62-450b-a811-46797762a2a8.1451149756383.ics");
-				// filename = args[0];
+		// Termine zwischen 27.12.2015 und 27.12. +20 Tagen
+		lowerdatetime = new DateTime(1451254583 * 1000);
+		upperdatetime = new DateTime(1452982583 * 1000);
+
+		String filename = new String();
 
 		// Locate the Jar file
 		FileSystemManager fsManager = VFS.getManager();
-		FileObject icsFile = fsManager.resolveFile("file:///home/christof/.calendars/christof/");
-		FileObject[] children = icsFile.getChildren();
+		// FileObject icsFile =
+		// fsManager.resolveFile("file:///home/christof/.calendars/christof/");
+		FileObject icsFile = fsManager.resolveFile(
+				"file:////home/christof/.calendars/christof/bd2bba8e-ba62-450b-a811-46797762a2a8.1451149756383.ics");
 
+		FileObject[] children = { icsFile };
+		if (icsFile.getType().toString().equals("folder")) {
+			children = icsFile.getChildren();
+		}
 		for (int f = 0; f < children.length; f++) {
 			System.out.println(children[f].getName().getPath());
 			filename = children[f].getName().getPath();
 
 			FileInputStream fin = new FileInputStream(filename);
 			CalendarBuilder builder = new CalendarBuilder();
+
+			System.setProperty("ical4j.unfolding.relaxed", "true");
+
 			Calendar calendar = builder.build(fin);
 
+			String vevent = new String("VEVENT");
 			for (final Component component : calendar.getComponents()) {
-				System.out.println("Component [" + component.getName() + "]");
+				// only work on VEVENTS, ignore VTODO
+				if (vevent.equals(component.getName())) {
+					System.out.println("Component [" + component.getName() + "]");
+					if (isinbetween(component, lowerdatetime, upperdatetime)) {
+						System.out.println("der folgende Termin liegt im Intervall:");
+						for (Property property : component.getProperties()) {
+							System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
+						}
+					}
 
-				for (final Property property : component.getProperties()) {
-					System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
+					System.out.println("RRULE: " + component.getProperties("RRULE") + " DTSTART: "
+							+ component.getProperties("DTSTART") + " EXRULE: " + component.getProperties("EXRULE")
+							+ " EXDATE: " + component.getProperties("EXDATE") + " RDATE: "
+							+ component.getProperties("RDATE"));
 				}
-				isinbetween(component, lowerdatetime, upperdatetime);
-
-				System.out.println(component.getProperties("RRULE"));
-				System.out.println(component.getProperties("DTSTART"));
-				System.out.println(component.getProperties("EXRULE"));
-				System.out.println(component.getProperties("EXDATE"));
-				System.out.println(component.getProperties("RDATE"));
 			}
 		}
 		// System.out.println(ip.get("RRULE"));
