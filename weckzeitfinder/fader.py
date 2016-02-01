2f7df9da5b03229680e6cf84c9cbc3c840bc0f75
root <root@snuselpi.christofschulze.com>
2016-02-01 02:36:44 +0100

    script to fade led strip to full brightness to simulate sunrise

--- weckzeitfinder/fader.py
+++ weckzeitfinder/fader.py
@@ -13,23 +13,52 @@ curval=0
 
 
 value=float(sys.argv[1])
-step=1
+dur=float(sys.argv[2])
 
 def changer(pin, amount):
     f = open('/dev/pi-blaster', 'w')
     f.write('%d=%s\n'%(pin, str(amount)))
 
-def fade(color, direction, curval, val, step):
-    multiplier = 100
-    if (abs(curval - val)* multiplier < 15):
-	multiplier = 1000
+def fade(color, curval, val):
+    step=1
     if curval > val:
         step=-step
-    delay = 0.5/(float(abs(curval - value)/abs(step))*multiplier)
+
+    while (curval <> val):
+	    multiplier = 3000
+	    secondbound = val
+            firstbound=curval
+	    if (curval >= 0.5):
+		multiplier = 100
+		if (step < 0):
+			secondbound=max(val,0.499)
+		else:
+			secondbound=val
+	    elif (curval >= 0.3):
+		multiplier = 1000
+		if (step < 0):
+			secondbound=max(val,0.299)
+		else:
+			secondbound=min(val,0.5)
+	    else:
+		multiplier=3000
+		if (step < 0):
+			secondbound=max(val,0)
+		else:
+			secondbound=min(val,0.3)
+	    
+	    fadeint(color, firstbound, secondbound, step, multiplier)
+	    curval=secondbound
+	    print curval
+
+		
+def fadeint(color, curval, val, step, multiplier) :   
+    delay = dur/(float(abs(curval - value)/abs(step))*multiplier)
     for i in range (int(multiplier * curval),  int(multiplier * val), int(step)):
         f=(i/float(multiplier))
         changer (color, f)
         sleep(delay)
+
     changer ( color, val)
 
 g = open('/tmp/' + str(white), 'r+')
@@ -39,4 +68,4 @@ g.write('%f'%value)
 g.truncate()
 g.close()
 if curval <> value:
-	fade(white, 'up', curval, value, step)
+	fade(white, curval, value)
